3149e3bc942eab9f21b1c2692607c07e936fa87b
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-12-02 19:19:30 +0100

    Copie du App.Vue du TD3

--- poke_bowl/src/router/index.ts
+++ poke_bowl/src/router/index.ts
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PokeBowl from '@/views/PokeBowl.vue';
 
-
 const router = createRouter({
   history: createWebHistory(''),
   routes: [
