18365a043ba4b57adfbe3d68ad893f42fe9f82d2
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-12-02 18:58:10 +0100

    Remsie du fichier des types

---
+++ poke_bowl/src/types.ts
@@ -0,0 +1,27 @@
+export interface Categorie{
+    id: number;
+    nom: string;
+    couleur: string;
+}
+
+export interface Utilisateur{
+    id: number;
+    login: string;
+    email: string;
+    password: string;
+    premium: boolean;
+}
+
+export interface Ingredient{
+    id: number;
+    nom: string;
+    type: Categorie;
+}
+
+export interface Recette {
+    id: number;
+    nom : string;
+    categorie : Categorie;
+    recommander : boolean;
+}
+
