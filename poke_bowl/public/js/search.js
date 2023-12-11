c253b791c64f7d77e8458b016bb88b2f7b56c46f
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2023-12-11 21:23:25 +0100

    Ajout des commentaires

--- poke_bowl/public/js/search.js
+++ poke_bowl/public/js/search.js
@@ -1,3 +1,8 @@
+/*
+    Attribut : 
+        - val : string, value à chercher dans la chaine
+        - proto : tableau contenant les objets ayant nom comme attribut dans lesquels chercher
+*/
 function search(val,proto){
     let liste = [];
     for(let valProto in proto){
