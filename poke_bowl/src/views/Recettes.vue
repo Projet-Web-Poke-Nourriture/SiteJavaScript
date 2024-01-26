9f5a524b051c4830f2ad5f3d6be8b604587c9ce9
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2024-01-26 10:42:22 +0100

    Ajout de la fonction de recherche, mais découverte d'un bug, toujours le meme quand on clique

--- poke_bowl/src/views/Recettes.vue
+++ poke_bowl/src/views/Recettes.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import BoiteRecette from "@/components/BoiteRecette.vue";
 import type { Recette } from "@/types";
-import { ref, type Ref } from "vue";
+import { ref, type Ref, watch } from "vue";
+import { loadScript } from "vue-plugin-load-script";
+import {useRouter} from 'vue-router';
+const router = useRouter();
 
 const recettes: Ref<Recette[]> = ref([
   {
@@ -85,8 +88,44 @@ const recettes: Ref<Recette[]> = ref([
     etapes: [{numero:1,descriptif:"passe"},{numero:2,descriptif:"passe2"}]
   },
 ]);
+
+loadScript('/js/search.js');
+
+const searchTerm = ref(''); // Terme de recherche lié à la saisie de l'utilisateur
+const filteredRecettes = ref<Recette[]>([]); // Résultats filtrés à afficher
+
+watch(searchTerm, (newValue) => {
+  if (newValue) {
+    // Mettre à jour filteredIngredients seulement si searchTerm n'est pas vide
+    filteredRecettes.value = search(newValue, recettes.value);
+  } else {
+    // Si searchTerm est vide, réinitialiser filteredIngredients à vide
+    filteredRecettes.value = [];
+  }
+});
+
+function selectIngredient(recette: Recette) {
+  // Action à effectuer, par exemple, naviguer vers une page de détail ou ajouter à une liste
+    router.push({ path: `/recette/${recette.id}` });
+}
 </script>
 
 <template>
+
+  <input type="text" id="recherche" name="recherche" v-model="searchTerm" />
+
+  <!-- Afficher les résultats seulement si searchTerm n'est pas vide -->
+  <div class="resultats-recherche" v-if="searchTerm && filteredRecettes.length">
+    <div
+      class="resultat"
+      v-for="recette in filteredRecettes"
+      :key="recette.id"
+      @click="selectIngredient(recette)"
+    >
+      {{ recette.nom }}
+    </div>
+  </div>
+
+
     <BoiteRecette v-for="recette in recettes" :key="recette.id" :recette="recette"/>
 </template>
