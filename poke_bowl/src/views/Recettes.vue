0bfe5b4c22fe160e82ec9d504b94c56f2fb970a0
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2024-01-27 15:02:42 +0100

    Correction d'erreur pour lier le form

--- poke_bowl/src/views/Recettes.vue
+++ poke_bowl/src/views/Recettes.vue
@@ -122,7 +122,7 @@ const goToRecettes = () => {
 <template>
   <div class="recette-search-create">
     <input type="text" id="recherche" name="recherche" v-model="searchTerm" placeholder="Rechercher une recette..." />
-    <button @click="goToFormRecette">Créer une recette</button>
+    <button @click="goToRecettes">Créer une recette</button>
   </div>
 
   <!-- Afficher les résultats seulement si searchTerm n'est pas vide -->
