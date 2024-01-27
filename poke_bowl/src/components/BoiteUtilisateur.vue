f8c6a01a48480e3dd2a6336700cc9b8a064a23a8
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2024-01-27 14:55:23 +0100

    Suppression de ligne qui créait des erreurs en console

--- poke_bowl/src/components/BoiteUtilisateur.vue
+++ poke_bowl/src/components/BoiteUtilisateur.vue
@@ -1,9 +1,6 @@
 <script setup lang="ts">
 import type { Utilisateur } from "@/types";
-import { loadScript } from "vue-plugin-load-script";
 import { md5 } from 'js-md5';
-
-let a = loadScript("/js/md5.js");
 
 const props = defineProps<{ utilisateur: Utilisateur}>();
 const md5hash = md5(props.utilisateur.email);
