92fe2c9bc06d00b7c5e626c812a58b243133f3f7
Mattdldm <matthieu.demontchalin@gmail.com>
2024-01-28 16:09:29 +0100

    Se connecter / deconnecter disparait en fonction de l'etat

--- poke_bowl/src/App.vue
+++ poke_bowl/src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import { useRouter } from "vue-router";
+
 const router = useRouter();
+const isLoggedIn = ref(false);
+
+const checkLoginStatus = () => {
+  isLoggedIn.value = !!localStorage.getItem('userToken');
+};
+
+onMounted(checkLoginStatus);
 
 const logout = () => {
   localStorage.removeItem('userToken');
+  checkLoginStatus();
   router.push('/connexion');
-}
+};
 </script>
 
 <template>
@@ -13,14 +23,12 @@ const logout = () => {
     <header>
       <h1 @click="router.push('/PokeBowl')">PokeBowl</h1>
       <nav>
-        <div @click="router.push({ name: 'AllIngredients' })">
-          Les ingrédients
-        </div>
+        <div @click="router.push({ name: 'AllIngredients' })">Les ingrédients</div>
         <div @click="router.push({ name: 'allUsers' })">Les utilisateurs</div>
         <div @click="router.push({ name: 'allRecettes' })">Les recettes</div>
         <div @click="router.push({ name: 'inscription' })">S'inscrire</div>
-        <div @click="router.push({ name: 'connexion' })">Se connecter</div>
-        <div @click="logout">Se déconnecter</div>
+        <div v-if="!isLoggedIn" @click="router.push({ name: 'connexion' })">Se connecter</div>
+        <div v-if="isLoggedIn" @click="logout">Se déconnecter</div>
       </nav>
     </header>
     <main>
