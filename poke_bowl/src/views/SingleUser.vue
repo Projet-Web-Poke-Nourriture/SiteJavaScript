7dab0185f1778ef769bd25b5aa8fd2e0aeedbf99
KyucraftV2 <killian.reiner@etu.umontpellier.fr>
2024-01-29 10:09:51 +0100

    Ajout de  la vue utilisateur selon l'API

--- poke_bowl/src/views/SingleUser.vue
+++ poke_bowl/src/views/SingleUser.vue
@@ -3,16 +3,21 @@ import BoiteUtilisateur from "@/components/BoiteUtilisateur.vue";
 import type { Utilisateur } from "@/types";
 import { type Ref, ref } from "vue";
 import { useRoute } from "vue-router";
+import { jwtDecode } from "jwt-decode";
+
 const route = useRoute();
 const id = route.params.id;
 
-const user: Ref<Utilisateur> = ref({
-  id: Number(id),
-  login: "Toto",
-  email: "toto@titi.fr",
-  password: "password",
-  premium: false,
-});
+const user = ref();
+const userToken = localStorage.getItem("userToken");
+let roles = new Proxy([],{});
+if (userToken) {
+  const decoded = jwtDecode(userToken);
+  user.value = decoded;
+  console.log(roles)
+}
+
+
 </script>
 
 <template>
